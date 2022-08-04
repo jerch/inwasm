09f5139a445a06c1de5aff74a9300821bdad0fe9
Jörg Breitbart <jerch@rockborn.de>
2022-08-04 16:15:48 +0200

    fix custom build script

--- testproject/custom/build.sh
+++ testproject/custom/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 # some custom build steps (just calling wat2wasm for simplicity)
-../node_modules/inwasm/node_modules/wabt/bin/wat2wasm module.wat
+if [ -d "../node_modules/inwasm/node_modules/wabt/bin/" ]; then
+  ../node_modules/inwasm/node_modules/wabt/bin/wat2wasm module.wat
+else
+  ../node_modules/wabt/bin/wat2wasm module.wat
+fi
