82f0627d79af6d9f56a2272605cf041f60cd33b9
Jörg Breitbart <jerch@rockborn.de>
2022-08-04 15:45:02 +0200

    setup gh action for install test

---
+++ testproject/install_fix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# remove locally linked inwasm from project and place packaged version instead
+sed 's/\"inwasm\": \"file:\.\.\/inwasm\"//g' package.json > tmp
+mv tmp package.json
+npm install inwasm    # grab latest package
+rf -rf node_modules   # reset folder to clean state
