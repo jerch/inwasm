a402484a9230f6ec06ede1ab855280bc097c4e86
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-19 15:08:26 +0800

    Delete inwasm directory

--- inwasm/src/runners/custom.ts
+++
@@ -1,8 +0,0 @@
-import { IMemorySettings, IWasmDefinition } from '..';
-
-
-export default function(def: IWasmDefinition, buildDir: string, filename: string, memorySettings: IMemorySettings): Uint8Array {
-  if (def.customRunner)
-      return def.customRunner(def, buildDir, filename, memorySettings);
-    throw new Error('no customRunner defined');
-}
