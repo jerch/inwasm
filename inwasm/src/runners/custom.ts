76dfe5f52e48bf561414e38a8660eb5c2b405090
Jörg Breitbart <jerch@rockborn.de>
2023-04-18 22:21:12 +0200

    cmdline skip flag, fixes #16

--- inwasm/src/runners/custom.ts
+++ inwasm/src/runners/custom.ts
@@ -2,10 +2,6 @@ import { IMemorySettings, IWasmDefinition } from '..';
 
 
 export default async function(def: IWasmDefinition, buildDir: string, filename: string, memorySettings: IMemorySettings): Promise<Uint8Array> {
-  // assume noCache by default for custom runners
-  if (def.noCache === undefined) {
-    def.noCache = true;
-  }
   if (def.customRunner) {
     return await def.customRunner(def, buildDir, filename, memorySettings);
   }
